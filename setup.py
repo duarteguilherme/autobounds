2035c205ae4d9a7171fcfc7c3654801be89ef74f
Guilherme Jardim Duarte <gjardimduarte@gmail.com>
2021-02-28 15:43:44 -0500

    add setup and fix toml bug add characters

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="causalid-gjduarte", # Replace with your own username
+    version="0.0.1",
+    author="Guilherme Duarte, Dean Knox, Jonathan Mummolo",
+    author_email="gjardimduarte@gmail.com",
+    description="A package that includes algorithms for causal identification",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
