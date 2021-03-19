11d33fbd32302eb93875ce25006c582245358305
Guilherme Jardim Duarte <gjardimduarte@gmail.com>
2021-03-18 21:02:05 -0400

    First version of SCMs with samples

--- causalid/SCM.py
+++ causalid/SCM.py
@@ -1,5 +1,31 @@
-from numpy import random
+import numpy as np
+from copy import deepcopy
+import pandas as pd 
 
+func_pool = [
+        lambda a,b: a & b,
+        lambda a,b: a | b,
+        lambda a,b: a ^ b,
+        lambda a,b:  ((a ^ b) - 1)*(-1)
+        ]
+
+
+def find_possible_functions(dag, v):
+    """ For a specific variable, 
+    return a possible f, based on the parents of v
+    and functions from 0 to 3
+    This functions is to be used i
+    in the context of from_dag method in SCM data structure
+    """
+    pa = dag.find_parents(v)
+    return ( 
+            tuple([ 'U_' + v ] +
+               [ 'U_' + x 
+                   for x in pa if x in dag.U] +
+               [  x 
+                   for x in pa if x in dag.V]),
+               tuple(x for x in np.random.randint(0,4,len(pa)))
+               )
 
 class SCM:
     """ 
@@ -18,60 +44,79 @@ class SCM:
     
     """
     def __init__(self):
-        self.u = set()
-        self.v = set()
-        self.f = {}
-        self.p = {}
+        self.U = set()
+        self.V = set()
+        self.F = {}
+        self.P = {}
         
-    def from_dag(self, dag = '', unob = ''):
+    def from_dag(self, dag):
         """ This function gets a DAG and returns 
-        a SCM, with whom that DAG is compatible. 
+        a SCM, with which that DAG is compatible. 
         The functions that control the behavior of each 
         V_i variable will be picked up from a pool of 
         'AND', 'OR', 'XOR' functions. 
     
         Examples: 
              scm = SCM()
-             scm.from_dag('X -> Y, U_xy -> X, U_xy -> Y', unob = 'U_xy')
+             dag = DAG()
+             dag.from_structure(('X -> Y, U_xy -> X, U_xy -> Y', unob = 'U_xy')
+             scm.from_dag(dag)
         """
-        pass
+        us = set(
+                [ 'U_' + x for x in dag.U  ] + 
+                [ 'U_' + x for x in dag.V  ] )
+        ps = np.random.uniform(0.2, 0.8, len(us))
+        for i in range(len(ps)):
+            self.set_u(us[i], ps[i])
+        vs = self.V
+        for v in vs:
+            self.set_v(v, find_possible_functions(dag, v))
     
     def set_u(self, u, p):
-        self.u.add(u)
-        self.p[u] = p
+        self.U.add(u)
+        self.P[u] = p
     
     def set_v(self, v, f):
         """ 
-        f must be a tuple with the first element being a function and the second one 
-        a unobservable """
+        f must be a tuple with the second element being a function numbers
+        and the first one 
+        the variables.
+        So for example, (('U_v', 'U_m_v', 'V'), (0,2)) is a valid 
+        function, to become a real function later
+        """
         if len(f) < 2:
             raise Exception("Arg 'f' requires a tuple with at least two elements")
-        if not hasattr(f[0], "__call__"):
-            raise Exception("Arg 'f' requires a first element of the tuple to be a function")
-        self.v.add(v)
-        self.f[v] = f
+        if ( len(f[0]) != (len(f[1]) + 1) ):
+            raise Exception("Number of functions must be equal to the number of variables minus 1")
+        self.V.add(v)
+        self.F[v] = f
     
     
-    def sample_v(self, N):
-        self.m = np.array([ self.f_m(self.u_m[i], self.u_mb[i])
-            for i in range(N) ]).reshape(-1)
-        self.b = np.array([ self.f_b(self.u_b[i], self.u_mb[i])
-            for i in range(N) ]).reshape(-1)
-        self.y = np.array([ self.f_y(self.m[i], self.b[i],self.u_y[i])
-            for i in range(N) ]).reshape(-1)
+    def sample_u(self, N):
+        self.u_data = {}
+        for u in self.U:
+            self.u_data[u] = np.random.binomial(1, p[u], N)
     
-    def draw_sample(self, N):
+    def draw_sample(self, intervention = {}):
         """ 
         Draw simulated sample according to the specified model.
         The size of the sample is specified through parameter N.
         The sample is a matrix (N, 3). The first column represents
         Y. The second and third ones represent M and B.
-        """
-        self.N = N
-        self.sample_u(N)
-        self.set_f()
-        self.sample_v(N)
         
-
-
+        argument intervention allows users to 
+        fix values for some of the variables.
+        """
+        dag = deepcopy(self.dag)
+        data = pd.DataFrame(self.u_data)
+        for key, values in intervention.keys():
+            dag.truncate(key)
+            data[key] = values
+        for i in dag.V:
+            data[i] = data[self.F[0][0]]
+            for j in range(len(self.F[1])):
+                data[i] = self.F[1][j](
+                        data[i], data[self.F[0][j + 1]]
+                        )
+        return data
 
