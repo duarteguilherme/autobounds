5f0fe74137364096da4538321a15122578536cab
Guilherme Jardim Duarte <gjardimduarte@gmail.com>
2021-04-24 22:33:23 -0400

    add method to add  indep constraints

--- autobound/bounds.py
+++ autobound/bounds.py
@@ -3,8 +3,6 @@ import numpy as np
 from itertools import product
 from functools import reduce
 
-##### IMPORTANT ***************************
-# We have to create test to check if find_parents is always in order
 
 func_r = lambda n: (np
         .array(list(product([0,1], repeat = 2**n)))
@@ -47,19 +45,27 @@ class causalProgram(object):
     def from_dag(self, dag):
         self.dag = dag
         self.c_comp = self.dag.find_c_components()
-        self.get_canonical_index(dag)
+        self.get_canonical_index()
         for c in self.c_comp:
             for x in self.get_parameters(c):
                 self.parameters[x] = self.program.addVar(x, vtype = "C",
                         lb = 0, ub = 1) 
    
     def set_obj(self, expre):
+        """
+        Input: an expression written in pyscipopt format
+        No output. Add objective into self.program
+        """
         self.objvar = self.program.addVar(name = "objvar",
                 vtype = "C", lb = None, ub = None)
         self.program.setObjective(self.objvar, self.sense)
         self.program.addCons(self.objvar == expre)
     
     def add_prob_constraints(self):
+        """ 
+        No input
+        Add all axioms of probability constraints
+        """
         c_components = [ '_'.join(x.split('_')[0:-1]) for x in self.parameters.keys() ]
         c_components = set(c_components)
         for c in c_components:
@@ -78,16 +84,36 @@ class causalProgram(object):
                     ''.join([ str(var[i]) for i in c])  )
         return factorization
     
+    def get_response_expr(self, var={}):
+        """ Differently from get_expr,
+        this gets probabilities over response variables,
+        and return equivalent expression.
+        For example, P(Ry=1) self.get_response_expr({"Y":3}), will 
+        return a sum of q_xy, for a c-component {X,Y}
+        (This expression accepts only one c-component)
+        """
+        relevant_c = [ c for c in self.c_comp 
+                if any([ i in c for i in var.keys()] )  ]
+        if len(relevant_c) > 1:
+            raise Expcetion("More than one c-component assigned.")
+        relevant_c = relevant_c[0]
+        parameters = []
+        for v in relevant_c:
+            if v in var.keys():
+                parameters.append([var[v]])
+            else:
+                parameters.append(list(range(2**(1+self.cn_index[v]))))
+        parameters = [ [ str(j) for j in i] for i in product(*parameters) ]
+        parameters = [ 'q_' + ''.join(relevant_c) + '_' + ''.join(i) 
+                for i in  parameters] 
+        return parameters
+    
     def get_expr(self, var = "", do = ""):
-        """ Get an expression and 
-        transform it to canonical form
-        var identifies the variables 
-        and do the interventions
+        """ Input: a probabilistic expression over V expression
+        Output: canonical form expression
         for example: P(Y=1|do(X=1)) can 
         become 
         program.define_expression(var = "Y=1", do = "X=1")
-        The algorithm works by getting a list 
-        of parameters and removing the unnecessary ones
         """
         if ( var == "" ):
             raise Exception("Error: specify v")
@@ -108,8 +134,10 @@ class causalProgram(object):
         return factorized
      
     def get_q_index(self,var, do = {}):
-        """ Var here is a dictionary 
-        For the model, all the variables must 
+        """ 
+       Input: ocurrence of u. program.get_q_index({'Z':1, 'X':0, 'Y':1})
+       Output: index. {'Z':[2,3], 'X':[0,1], 'Y': [2,3]}
+        All the variables must 
         be represented.
         For example, program.get({'X': 1, 'Y': 0})
         """
@@ -124,21 +152,86 @@ class causalProgram(object):
         return q_index
     
     def get_parameters(self, c):
+        """ 
+        Input: c-component (frozenset)
+        Output: tuple of all parameters of this c-component
+        """
         q_values = product(*[ [ str(x) for x in range(2**(1+self.cn_index[v]))] for v in c ])
         params = tuple(['q_' + ''.join(c) + '_' + ''.join(q) for q in q_values])
         return params
     
-    def get_canonical_index(self, dag):
+    def get_canonical_index(self):
+        """
+        Create self.cn_index 
+        with canonical index for each variable in the DAG
+        """
         self.cn_index = {}
         for v in self.dag.V:
             self.cn_index[v] = len(
-                    [i for i in self.dag.find_parents(v) 
-            if i not in self.dag.U ])
+                    [i for i in self.dag.find_parents_no_u(v) ])
     
-    def set_objective(self, expr):
-        pass
+    def check_indep(self, c):
+        """
+        In a certain c-component 'c',
+        check for possible independencies among 
+        response variables
+        ------
+        Input: c-component
+        Output: independent response variable tuples
+        """
+        c = list(c)
+        if len(c) < 3:
+            return []
+        res = []
+        for i in range(len(c)-1):
+            for j in range(i+1, len(c)):
+                if len(
+                        self.dag.find_parents_u(c[i]).intersection(
+                        self.dag.find_parents_u(c[j])
+                        )) == 0:
+                    res.append({c[i],c[j]})
+        return res
     
-    def set_constraint(self, expr, result):
-        pass
+    def add_indep(self, var):
+        """ 
+        Input: Var
+        This method will be called by add_indeps in order 
+        to simplify code. 
+        Independences for particular values will be added as constraints
+        """
+        keys = list(var.keys())
+        cons1 = []
+        for i in [0,1]:
+            cons1.append(quicksum([ self.parameters[k]
+                    for k in 
+                    self.get_response_expr({keys[i]: var[keys[i]]}) ] ))
+        cons2 = quicksum([ (-1)*self.parameters[k]
+                for k in 
+                self.get_response_expr(var) ])
+        self.addCons(cons1[0]*cons1[1] - cons2 == 0)
+    
+    def add_rest_indep(self, indep):
+        indep = list(indep)
+        elem_1 = 2**(1+self.cn_index[indep[0]])
+        elem_2 = 2**(1+self.cn_index[indep[1]])
+        for i in range(elem_1):
+            for j in range(elem_2):
+                self.add_indep({indep[0]: i, indep[1]: j})
+    
+    def add_indep_constraints(self):
+        """ For each components, check independencies 
+        among variables and add them as constraints
+        to the model. 
+        """
+        indeps = []
+        for c in self.c_comp:
+            indeps = indeps + self.check_indep(c)
+        for i in indeps:
+            print(i)
+            self.add_rest_indep(i)
+    
 
-        
+
+
+
+
