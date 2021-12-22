9b182d5cbbd84cb410514a9e701fe25fc09e4f82
Guilherme Duarte <gjardimduarte@gmail.com>
2021-12-21 19:51:38 -0500

    create classes for canonical model and causal program

---
+++ tests/test_program.py
@@ -0,0 +1,15 @@
+from autobound.autobound.program import program
+import numpy.random as rd
+import pytest
+import os
+
+
+def test_fromdag():
+    x = causalProgram()
+    x.from_dag(
+            DAG().from_structure("U -> X, X -> Y, U -> Y, Uy -> Y", unob = "U , Uy")
+            )
+    assert x.V == set(('Y', 'X'))
+    assert x.E == set((('Uy', 'Y'), ('X','Y'),('U','Y'),('U','X')))
+    assert x.U == set(('Uy','U'))
+
