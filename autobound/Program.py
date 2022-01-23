c11db2022e3da228707b2ccdd763417c2d5e0656
Guilherme Jardim Duarte <gjardimduarte@gmail.com>
2022-01-23 11:59:49 -0500

    add to_pip method

--- autobound/Program.py
+++ autobound/Program.py
@@ -10,14 +10,28 @@ class Program:
     """
     def __init__(self):
         self.parameters = [ ]
-        self.estimand = tuple()
         self.constraints = [ tuple() ]
     
     def to_pyomo(self):
         pass
     
-    def to_pip(self):
-        pass
+    def to_pip(self, filename, sense = 'max'):
+        filep = open(filename, 'w')
+        sense = 'MAXIMIZE' if sense == 'max' else 'MINIMIZE'
+        filep.write(sense + '\n' + '  obj: objvar' + '\n')
+        filep.write('\nSUBJECT TO\n')
+        for i, c in enumerate(self.constraints):
+            fipep.write(f'  a{i}: ' + ' + '.join([ 
+                str(k[0]) * ' * '.join(k[1])   
+                for k in c ]) + '\n') 
+        filep.write('\nBOUNDS\n')
+        for p in self.parameters:
+            if p != 'objvar':
+                filep.write(f'  0 <= {p} <= 1\n')
+            else:
+                filep.write(f'  -1 <= {p} <= 1\n')
+        filep.write('\nEND')
+        filep.close()
     
     def to_cip(self):
         pass
