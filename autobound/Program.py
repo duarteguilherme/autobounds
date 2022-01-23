f425d325434b774f73120e18f1ede94a083fb4ee
Guilherme Duarte <gjardimduarte@gmail.com>
2022-01-23 14:57:10 -0500

    fix bug related to name of filep

--- autobound/Program.py
+++ autobound/Program.py
@@ -1,3 +1,45 @@
+from autobound.causalProblem import causalProblem
+from autobound.DAG import DAG
+import io 
+
+def test_pip_join_expr():
+    assert pip_join_expr(['0.5', 'X00.Y00'], ['X00.Y00', 'Z1', 'Z0']) == '0.5 X00.Y00'
+    assert pip_join_expr(['0.5'], ['X00.Y00', 'Z1', 'Z0']) == '0.5'
+    assert pip_join_expr(['X00.Y00'], ['X00.Y00', 'Z1', 'Z0']) == 'X00.Y00'
+    assert pip_join_expr(['0.5', 'X00.Y00', 'Z1'], ['X00.Y00', 'Z1', 'Z0']) == '0.5 X00.Y00 * Z1'
+
+def pip_join_expr(expr, params):
+    """ 
+    It gets an expr and if there is a coefficient, it 
+    separates without using * .
+    It is required as a simple list join is insufficient 
+    to put program in pip format
+    """
+    coef = ''.join([x for x in expr if x not in params ])
+    expr_rest = ' * '.join([ x for x in expr if x in params ])
+    coef = coef + ' ' if coef != '' and expr_rest != '' else coef 
+    return coef + expr_rest
+
+def test_program_iv():
+    dag = DAG()
+    dag.from_structure("Z -> X, X -> Y, U -> X, U -> Y", unob = "U")
+    problem = causalProblem(dag)
+    datafile = io.StringIO('''X,Y,Z,prob
+    0,0,0,0.05
+    0,0,1,0.05
+    0,1,0,0.1
+    0,1,1,0.1
+    1,0,0,0.15
+    1,0,1,0.15
+    1,1,0,0.2
+    1,1,1,0.2''')
+    problem.set_estimand(problem.query('Y(X=1)=1') + problem.query('Y(X=0)=1', -1))
+    problem.constraints
+    problem.load_data(datafile)
+    problem.add_prob_constraints()
+    z = problem.write_program()
+    z.to_pip('/home/beta/test_iv.pip')
+
 class Program:
     """ This class
     will state a optimization program
@@ -21,9 +63,9 @@ class Program:
         filep.write(sense + '\n' + '  obj: objvar' + '\n')
         filep.write('\nSUBJECT TO\n')
         for i, c in enumerate(self.constraints):
-            fipep.write(f'  a{i}: ' + ' + '.join([ 
-                str(k[0]) * ' * '.join(k[1])   
-                for k in c ]) + '\n') 
+            filep.write(f'  a{i}: ' + ' + '.join([ 
+               pip_join_expr(k, self.parameters) for k in c ]) 
+                + ' = 0\n') 
         filep.write('\nBOUNDS\n')
         for p in self.parameters:
             if p != 'objvar':
