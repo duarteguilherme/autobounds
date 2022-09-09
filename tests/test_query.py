091dd3fd31b3e894928e3bbfa31366607e4fb27e
Guilherme Duarte <gjardimduarte@gmail.com>
2022-09-08 22:37:25 -0400

    tests for query passed --- have to fix causalProblem

--- tests/test_query.py
+++ tests/test_query.py
@@ -8,18 +8,29 @@ def test_query():
     y.from_structure("Z -> X, X -> Y, U -> X, U -> Y", unob = "U")
     x = causalProblem(y, {'X': 2})
     z = Parser(y)
-    print(x.query('Y=1') + x.query('Y=0'))
-    assert x.query('Y=1') + x.query('Y=0', -1) == x.query('Y=1') - x.query('Y=0')
-#    x.set_estimand(x.query('Y(X=1)=1&X=0') + x.query('Y(X=0)=1&X=0', -1), div = x.query('X=0'))
+    # Test equality 
+    assert x.query('Y=1') == x.query('Y=1')
+    assert not (x.query('Y=1') == x.query('Y=0'))
+    # Test subtraction and addition
+    assert (x.query('Y=1') + x.query('Y=0', -1)) == (x.query('Y=1') - x.query('Y=0'))
 
+
+def test_types():
+    assert Query('objvar') == Query([(1, ['objvar'])])
+    assert Query(0.19) == Query([(0.19, ['1'])])
+    assert Query(int('9')) == Query([(int(9), ['1'])])
+
+
+def test_mul():
+    assert (Query('X0.Y00') * Query('X1.Y11') * Query(0.29)) == Query([(0.29, ['X0.Y00', 'X1.Y11'])])
 
 def test_clean_query():
     duplicated_query = Query(  [(1, ['X00.Y10', 'Z0']), (1, ['X00.Y10', 'Z0']) ] )
     unordered_query = Query(  [(1, ['Z0','X00.Y11']), (1, ['X00.Y10', 'Z0']) ] )
     zero_query = Query(  [(1, ['Z0','X00.Y11']), (-1, ['X00.Y11', 'Z0']) ] )
-    duplicated_query = clean_query(duplicated_query)
-    unordered_query = clean_query(unordered_query)
-    zero_query = clean_query(zero_query)
+    duplicated_query.clean()
+    unordered_query.clean()
+    zero_query.clean()
     assert unordered_query[:] == [(1, ['X00.Y11', 'Z0']), (1, ['X00.Y10', 'Z0'])]
     assert duplicated_query[:] == [(2, ['X00.Y10', 'Z0'])]
     assert zero_query[:] == []
