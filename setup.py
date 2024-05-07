46e0e512b4a52fb4ff45d387b1cc190d6aaf0f2f
Guilherme Duarte <gjardimduarte@gmail.com>
2024-05-07 13:34:52 -0400

    add tqdm to setup

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
     install_requires = [
+        "tqdm",
         "setuptools",
         "pandas",
         "networkx",
