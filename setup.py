5a9d66f7315b1d8695c0fad042c22196061f93d7
Guilherme Duarte <gjardimduarte@gmail.com>
2024-05-07 00:26:01 -0400

    change requirements of setup.py

--- setup.py
+++ setup.py
@@ -22,4 +22,16 @@ setuptools.setup(
     ],
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
+    install_requires = [
+        "setuptools",
+        "pandas",
+        "networkx",
+        "matplotlib",
+        "wheel",
+        "numpy",
+        "pandas",
+        "plotnine",
+        "pyscipopt==4.4",
+        "sympy",
+        "scipy"]
 )
