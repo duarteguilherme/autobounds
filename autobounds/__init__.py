ccd0c7f362c250644089fdcc35bc860fb9b72fbf
Guilherme Duarte <gjardimduarte@gmail.com>
2025-01-04 13:23:25 -0500

    Add Query to general classes

--- autobounds/__init__.py
+++ autobounds/__init__.py
@@ -2,5 +2,6 @@ from .DAG import DAG
 from .causalProblem import causalProblem
 from .Parser import Parser
 from .Program import Program
+from .Query import Query
 
-__all__ = [ "DAG", "causalProblem", "Parser", "Program" ]
+__all__ = [ "DAG", "causalProblem", "Parser", "Program", "Query" ]
