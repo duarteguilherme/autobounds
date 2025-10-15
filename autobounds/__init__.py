f15fa8058e774bf55bcbbc350f1f9ebe453c143c
Guilherme Duarte <gjardimduarte@gmail.com>
2025-10-15 19:05:30 -0400

    respect_to imported without speficic call import

--- autobounds/__init__.py
+++ autobounds/__init__.py
@@ -3,6 +3,18 @@ from .causalProblem import causalProblem, respect_to
 from .Parser import Parser
 from .Program import Program
 from .Q import Q, Query
+from collections import namedtuple
+import platform
 
 
-__all__ = [ "DAG", "causalProblem", "Parser", "Program", "Query", "Q" ]
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial") # machine readable version info
+version_info = VersionInfo(0, 0, 1, "alpha", 0)
+version = f"{version_info.major}.{version_info.minor}.{version_info.micro} ({platform.python_implementation()} {platform.python_version()})"
+__version__ = f"{version_info.major}.{version_info.minor}.{version_info.micro}"
+
+
+__all__ = [ 
+    "version",
+    "version_info",
+    "__version__",
+           "DAG", "causalProblem", "Parser", "Program", "Query", "Q", "respect_to" ]
